f0aa0a904d1b399999b801a7a441100ff663950a
Duzasowa <harencko.vitaliy@gmail.com>
2021-12-28 20:50:57 +0100

    Middleware checking user role

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -7,7 +7,7 @@ module.exports = function (req, res, next) {
     try {
         const token = req.headers.authorization.split(' ')[1] // Bearer 
         if (!token) {
-            return res.status(401).json({message: "Not authorized"})
+          return res.status(401).json({message: "Not authorized"})
         }
         const decoded = jwt.verify(token, process.env.SECRET_KEY)
         req.user = decoded
