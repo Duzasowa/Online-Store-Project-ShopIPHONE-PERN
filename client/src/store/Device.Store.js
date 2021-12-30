9ae2ed472e5f60a781459da2390dcd8a3801c8d0
Duzasowa <harencko.vitaliy@gmail.com>
2021-12-30 21:32:40 +0100

    Creation of a page, a list of products, types, brands

--- client/src/store/Device.Store.js
+++ client/src/store/Device.Store.js
@@ -4,18 +4,28 @@ export default class DeviceStore {
   constructor() {
     this._types = [
       {id: 1, name: 'Holodylnik'},
-      {id: 2, name: 'Smartfony'}
+      {id: 2, name: 'Smartfony'},
+      {id: 3, name: 'Laptopy'},
+      {id: 4, name: 'TV'}
     ]
-    this._brands = [
+    this._brands = [ 
       {id: 1, name: "Samsung"},
-      {id: 2, name: "Apple"}
+      {id: 2, name: "Apple"},
+      {id: 3, name: "Lenovo"},
+      {id: 4, name: "Asus"}
     ]
     this._devices = [
       {id: 1, name: "Iphone 12 pro", price: 11000, rating: 3, img: `https://www.purposechurch.com/wp-content/uploads/2017/10/fpo400x300.png`},
-      {id: 1, name: "Iphone 12 pro", price: 11000, rating: 3, img: `https://www.purposechurch.com/wp-content/uploads/2017/10/fpo400x300.png`},
-      {id: 1, name: "Iphone 12 pro", price: 11000, rating: 3, img: `https://www.purposechurch.com/wp-content/uploads/2017/10/fpo400x300.png`},
-      {id: 1, name: "Iphone 12 pro", price: 11000, rating: 3, img: `https://www.purposechurch.com/wp-content/uploads/2017/10/fpo400x300.png`},
+      {id: 2, name: "Iphone 12 pro", price: 11000, rating: 3, img: `https://www.purposechurch.com/wp-content/uploads/2017/10/fpo400x300.png`},
+      {id: 3, name: "Iphone 12 pro", price: 11000, rating: 3, img: `https://www.purposechurch.com/wp-content/uploads/2017/10/fpo400x300.png`},
+      {id: 4, name: "Iphone 12 pro", price: 11000, rating: 3, img: `https://www.purposechurch.com/wp-content/uploads/2017/10/fpo400x300.png`},
+      {id: 5, name: "Iphone 12 pro", price: 11000, rating: 3, img: `https://www.purposechurch.com/wp-content/uploads/2017/10/fpo400x300.png`},
+      {id: 6, name: "Iphone 12 pro", price: 11000, rating: 3, img: `https://www.purposechurch.com/wp-content/uploads/2017/10/fpo400x300.png`},
+      {id: 7, name: "Iphone 12 pro", price: 11000, rating: 3, img: `https://www.purposechurch.com/wp-content/uploads/2017/10/fpo400x300.png`},
     ]
+    
+    this._selectedType = {}
+    this._selectedBrand = {}
     makeAutoObservable(this)
   }
 
@@ -29,6 +39,21 @@ export default class DeviceStore {
     this._devices = devices
   }
 
+  setSelectedType(type) {
+    this.setPage(1)
+    this._selectedType = type
+  }
+  setSelectedBrand(brand) {
+      this.setPage(1)
+      this._selectedBrand = brand
+  }
+  setPage(page) {
+      this._page = page
+  }
+  setTotalCount(count) {
+      this._totalCount = count
+  }
+
 
   get types() {
     return this._types
@@ -39,5 +64,19 @@ export default class DeviceStore {
   get devices() {
     return this._devices
   }
-  
+  get selectedType() {
+    return this._selectedType
+  }
+  get selectedBrand() {
+    return this._selectedBrand
+  }
+  get totalCount() {
+    return this._totalCount
+  }
+  get page() {
+    return this._page
+  }
+  get limit() {
+    return this._limit
+  }
 }
