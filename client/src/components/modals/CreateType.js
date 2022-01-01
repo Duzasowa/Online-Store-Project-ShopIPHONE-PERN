d010fc9fafaef06812a0ac400ce9a22c5c1c2267
Duzasowa <harencko.vitaliy@gmail.com>
2022-01-01 20:28:22 +0100

    Revive the ability to add new devices through the "Admin Panel"

--- client/src/components/modals/CreateType.js
+++ client/src/components/modals/CreateType.js
@@ -1,32 +1,44 @@
-import React from "react";
-import { Modal, Button, Form } from "react-bootstrap";
+import React, {useState} from 'react';
+import Modal from "react-bootstrap/Modal";
+import {Form, Button} from "react-bootstrap";
+import {createType} from "../../http/deviceAPI";
 
 const CreateType = ({show, onHide}) => {
-  return (
-    <Modal
-      show={show}
-      onHide={onHide}
-      size="lg"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Add new type
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
-          <Form.Control
-            placeholder={'Enter type names'}
-          />
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="outline-danger" onClick={onHide}>Close</Button>
-        <Button variant="outline-success" onClick={onHide}>Add</Button>
-      </Modal.Footer>
-    </Modal>
-  );
+    const [value, setValue] = useState('')
+
+    const addType = () => {
+        createType({name: value}).then(data => {
+            setValue('')
+            onHide()
+        })
+    }
+
+    return (
+        <Modal
+            show={show}
+            onHide={onHide}
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                  Add type
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Control
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                        placeholder={"Enter the type name"}
+                    />
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="outline-danger" onClick={onHide}>Close</Button>
+                <Button variant="outline-success" onClick={addType}>Add</Button>
+            </Modal.Footer>
+        </Modal>
+    );
 };
 
 export default CreateType;
