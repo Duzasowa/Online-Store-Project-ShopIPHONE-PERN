b051e3e136ba9997facc9d8d558c8a9640e765fd
Duzasowa <harencko.vitaliy@gmail.com>
2022-01-11 00:24:18 +0100

    NavBar update. V1.2.1

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,13 +1,11 @@
 import React, {useContext} from 'react';
 import {Context} from "../index";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import {NavLink} from "react-router-dom";
-import {Button} from "react-bootstrap";
+import {NavLink, Button, Nav, Navbar} from "react-router-dom";
 import { ADMIN_ROUTE, SHOP_ROUTE, LOGIN_ROUTE } from '../utils/consts';
 import {observer} from "mobx-react-lite";
 import Container from "react-bootstrap/Container";
 import { useHistory } from 'react-router-dom';
+import '../style/navbar.css';
 
 const NavBar = observer(() => {
   const {user} = useContext(Context)
@@ -19,32 +17,38 @@ const NavBar = observer(() => {
   }
 
   return (
-    <Navbar bg="dark" variant="dark">
-      <Container>
-          <NavLink style={{color:'white'}} to={SHOP_ROUTE}>VRSHOP</NavLink>
-          {user.isAuth ?
-            <Nav className="ms-auto">
-              <Nav.Link 
-                style={{color: 'white'}} 
-                onClick={() => history.push(ADMIN_ROUTE)} 
-              >
-                Admin Panel
-              </Nav.Link>
-              <Nav.Link 
-                style={{color: 'white'}} 
-                onClick={() => logOut()}
-              >
-                Log off
-              </Nav.Link>
-            </Nav>
-            :
-            <Nav className="ms-auto" style={{color: 'white'}}>
-              <Button variant={"outline-light"} onClick={() => history.push(LOGIN_ROUTE)}>Authorization</Button>
-            </Nav>
-          }
-      </Container>
-    </Navbar>
+    <div class="header">
+      {/* /-------------The upper part of Navbar-----------/ */}
+      <div class="header_wrapper">
+        <div class="header_top">
+          <a href="/" class="header_title">
+            <div class="header_icon">
+              <div class="header_name">ShopIPHONE</div>
+            </div>
+          </a>
+          <div class="header_search">
+            <input className="header__searchInput" type="text" placeholder="KuuuuuuklllaaA"/>
+          </div>
+        </div>
 
+        {/* /---------------The lower part of Navbar-----------/ */}
+        <div class="header_desktop">
+        <div class="header_navigation">
+          
+            <div class="header_navigation_menu">
+              <div class="header_button_menu">
+                <a class="menu_button">Online consulting</a>
+                <a class="menu_button">Purchase and financing</a>
+                <a class="menu_button">Servis</a>
+                <a class="menu_button">Technologies and innovations</a>
+              </div>
+            </div>
+        
+        </div>
+        </div>
+
+      </div>
+    </div>
   );
 });
 
